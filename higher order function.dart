c9c509ec1f00aecc5af73581c3236834b64bdb80
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:42:03 +0700

    Create higher order function.dart

---
+++ higher order function.dart
@@ -0,0 +1,18 @@
+//INI ADALAH CONTOH HIGHER ORDER FUNCTION
+// void halo(String nama, String Function(String) filter){
+//   var filternama = filter(nama);
+//   print('halo $filternama');
+// }
+
+// String filterkatakasar(String nama){
+//   if(nama == 'BABI'){
+//     return '***';
+//   }else{
+//     return nama;
+//   }
+// }
+
+// void main(){
+//   halo('ASEP', filterkatakasar);
+//   halo('BABI', filterkatakasar);
+// }
